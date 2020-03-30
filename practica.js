e7a1446e02310571df412d803148fe9596c4bc82
christian-cell <61358110+christian-cell@users.noreply.github.com>
2020-03-30 16:30:41 +0200

    snap de la parctica de bucles 2

---
+++ practica.js
@@ -0,0 +1,7 @@
+//solicito a la consola 50 numeros al azar mediante el siguiente comando
+var num = parseInt(Math.random()*100);
+console.log(num);
+//introduzco en una matriz llamada "arrayImpares"los numeros impares obtenidos.
+var arrayImpares = [35,43,41,31,7,91,93,73,69,53,73,49,15,91,17,59,89,39,67,89,77,29,67,59,93];
+//introduzco en una matriz llamada "arrayPares"los numeros pares obtenidos.
+var arrayPares = [58,52,84,16,42,38,78,94,30,28,74,64,26,32,64,60,10,32,38,40,8,40,62,22];
